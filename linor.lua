cef600a139267feef06658df31fd26c00ff6f754
etonormalno <86052185+etonormalno@users.noreply.github.com>
2023-07-26 23:09:55 +0300

    Update linor.lua

--- linor.lua
+++ linor.lua
@@ -32,12 +32,12 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(255, 21, 0);
+    AccentColor = Color3.fromRGB(252, 127, 3);
     OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
-    Font = Enum.Font.Code,
+    Font = Enum.Font.Gotham,
 
     OpenedFrames = {};
     DependencyBoxes = {};
@@ -3607,7 +3607,7 @@ function Library:CreateWindow(...)
             if Input.UserInputType == Enum.UserInputType.Keyboard and Input.KeyCode.Name == Library.ToggleKeybind.Value then
                 task.spawn(Library.Toggle)
             end
-        elseif Input.KeyCode == Enum.KeyCode.RightControl or (Input.KeyCode == Enum.KeyCode.RightShift and (not Processed)) then
+        elseif Input.KeyCode == Enum.KeyCode.Delete or (Input.KeyCode == Enum.KeyCode.P and (not Processed)) then
             task.spawn(Library.Toggle)
         end
     end))
