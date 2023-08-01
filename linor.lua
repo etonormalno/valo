4fb197d83e56700fa9c8a93f55e027613c35eee9
etonormalno <86052185+etonormalno@users.noreply.github.com>
2023-08-01 11:58:00 +0300

    Update linor.lua

--- linor.lua
+++ linor.lua
@@ -1,6 +1,6 @@
 local InputService = game:GetService('UserInputService');
 local TextService = game:GetService('TextService');
-local CoreGui = "msdrop";
+local CoreGui = game:GetService('CoreGui');
 local Teams = game:GetService('Teams');
 local Players = game:GetService('Players');
 local RunService = game:GetService('RunService')
@@ -14,10 +14,8 @@ local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 local ScreenGui = Instance.new('ScreenGui');
 ProtectGui(ScreenGui);
 
-print(CoreGui)
-
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global;
---ScreenGui.Parent = CoreGui;
+ScreenGui.Parent = CoreGui;
 
 local Toggles = {};
 local Options = {};
@@ -34,7 +32,7 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(252, 127, 3);
+    AccentColor = Color3.fromRGB(79, 255, 182);
     OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
