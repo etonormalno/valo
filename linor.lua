0806398e6830cc66efe605cbccf63abf0bf3f96c
etonormalno <86052185+etonormalno@users.noreply.github.com>
2023-08-05 15:29:23 +0300

    Update linor.lua

--- linor.lua
+++ linor.lua
@@ -32,7 +32,8 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(79, 255, 182);
+    --AccentColor = Color3.fromRGB(79, 255, 182);
+    AccentColor = Color3.fromRGB(255, 56, 56);
     OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
